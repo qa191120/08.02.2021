6656febd0db23549622b10bb313e3fa3589bc385
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-02-08 19:47:52 +0200

    Create oop_this_m.java

---
+++ oop_this_m.java
@@ -0,0 +1,17 @@
+        // Class car1
+        // private members:
+        // model, brand, year, color
+        // @Getter @Setter
+        // ctor - write yourself
+        //  call this.Go();
+        // this.model = model ...
+        // Go() { print ... }
+
+        // Class car2
+        // private members:
+        // m_model, m_brand, m_year, m_color
+        // @Getter @Setter
+        // ctor - write yourself
+        //  call this.Go();
+        // m_model = model ...
+        // Go() { print ... }
