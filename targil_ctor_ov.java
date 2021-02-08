d34d385bd7969da422dd4881f4596f806dc41831
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-02-08 20:18:23 +0200

    Create targil_ctor_ov.java

---
+++ targil_ctor_ov.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        // Mobile phone class:
+        // @Getter @Setter m_brand
+        // @Getter @Setter m_model
+        // @Getter @Setter m_price
+        // @Getter @Setter m_size
+        // -- ctor1 for all 4 (use this._ = _)
+        // -- ctor2 m_brand, m_model: call ctor 1 with size 5.4
+        // -- ctor3 m_brand : call ctor 1 with model 'standard' size 5.4
+        // -- ctor4 call ctor 1 with brand 'samsung' model 'standard' size 5.4
+        // add member m_color
+        // -- ctor 5 which gets all arguments:
+        //    call ctor 1 with 4 arguments
+        //    then sets the m_color from the argument
+
+    }
+}
